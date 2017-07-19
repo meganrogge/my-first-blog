ae43080a548c2896ac705fe5f1cc8094d26852ae
meganrogge <megrogge@gmail.com>
2017-07-18 20:45:59 -0700

    changed database settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,13 +75,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "megan",
-        "USER": "djangogirls",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
